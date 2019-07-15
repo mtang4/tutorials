31929482579a1652df8757886e984e75d5d700b7
mtang4 <52719555+mtang4@users.noreply.github.com>
2019-07-15 11:31:55 -0400

    Update cifar10_tutorial.py

--- beginner_source/blitz/cifar10_tutorial.py
+++ beginner_source/blitz/cifar10_tutorial.py
@@ -83,27 +83,27 @@ classes = ('plane', 'car', 'bird', 'cat',
 ########################################################################
 # Let us show some of the training images, for fun.
 
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+# import numpy as np
 
-# functions to show an image
-
-
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+# # functions to show an image
 
 
-# get some random training images
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
+# def imshow(img):
+#     img = img / 2 + 0.5     # unnormalize
+#     npimg = img.numpy()
+#     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+#     plt.show()
 
-# show images
-imshow(torchvision.utils.make_grid(images))
+
+# # get some random training images
+# dataiter = iter(trainloader)
+# images, labels = dataiter.next()
+
+# # show images
+# imshow(torchvision.utils.make_grid(images))
 # print labels
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 
 ########################################################################
